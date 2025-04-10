13ff4f31d950e95cd1b73f6e63d3d1735699f4d6
Shees Ikram <83660500+Sheesikram@users.noreply.github.com>
2025-04-10 13:38:50 +0500

    Add files via upload

--- Python/Files.py
+++ Python/Files.py
@@ -12,3 +12,32 @@ except:
 for line in fhand:
     line = line.rstrip()       # Remove trailing newline/whitespace
     print(line.upper())        # Convert to uppercase and print
+
+
+#for looking specific word
+# Use the file name mbox-short.txt as the file name
+fname = input("Enter file name: ")
+
+try:
+    fh = open(fname)
+except:
+    print("File cannot be opened:", fname)
+    quit()
+
+count = 0
+total = 0.0
+
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:"):
+        continue
+    line = line.strip()
+    colon_pos = line.find(':')
+    number = float(line[colon_pos + 1:])
+    total += number
+    count += 1
+
+if count > 0:
+    average = total / count
+    print("Average spam confidence:", average)
+else:
+    print("No matching lines found.")
